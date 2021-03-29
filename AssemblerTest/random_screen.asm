badf23747d8300745f14d5536d45a0eea9baa5e7
Markus Rührmair <markus.ruehrmair@gmail.com>
2021-03-29 21:45:43 +0200

    [AssemblerTest] random numbers subroutine

--- AssemblerTest/random_screen.asm
+++ AssemblerTest/random_screen.asm
@@ -3,13 +3,15 @@
 ;in that randomly determined screen position
 ;
 ;*** Variablen
-SCREEN  = $0428         ;Start position of the screen
-CHAR    = $18        ;X char
-
+SCREENMIN  = #$0428         ;Start position of the screen value
+SCREENMAX  = #$07E7         ;End position of the screen value
+SCREEN = $0428 ; screen position
+CHAR    = $18           ;X char
+CHROUT  = $FFD2 ; kernal table for Write byte to default output.
 ;LOOPCOUNTER = $6fff     ; loop counter register
  
 ;*** Startadresse BASIC-Zeile
-*=$7000
+*=$7000 ;
 ;       !byte $0c,$08,$e2,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
 
 ;Random noise with SID
@@ -18,28 +20,37 @@ CHAR    = $18        ;X char
          STA $D40F ; voice 3 frequency high byte
          LDA #$80  ; noise waveform, gate bit off
          STA $D412 ; voice 3 control register
+
+;clear screen
+         LDA #$93
+         JSR CHROUT
          
 ;loop counter (POKED in basic to #7000 => 28672) 
 ;         LDA #5
 ;         STA LOOPCOUNTER 
          
 ;start of screen population    => 28685     
-         LDA #CHAR ; load char
-         
+         LDX SCREENMIN
+         LDY SCREENMAX
+                  
 loop             
-         LDY $D41B ; Load rnd into y 
+         JSR rndpos
          LDX SCREEN,y ; load value from randomly determined address into x
          CPX #CHAR ;compare x with CHAR
          BEQ loop; if equal try again
-         STA SCREEN,y ; Print to screen if not equal
-
-;         LDX LOOPCOUNTER ; loop counter
-;loop2     
-;         LDY $D41B ; Load rnd into y
-;         STA SCREENLOWER,y ; Print to screen
-;         NOP
-;         NOP
-;         NOP
-;         DEX
-;         BNE loop2     
+         LDA #CHAR ; load char
+         STA y ; Print to screen if not equal
+;         LDX LOOPCOUNTER ; loop counter    
          RTS
+;****sub routine determine random screen position
+;X:Min screen
+;Y:Max screen
+;Output: Y random screen pos between min and max
+rndpos
+        LDA SCREENMIN ; Load screen min into a
+        CLC
+        ADC $D41B ; add random number
+        CMP SCREENMAX
+        
+        
+          
