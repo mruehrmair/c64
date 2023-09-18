fcc70d092bc0b90e40f2618ddc1d19283f5ed5fe
Markus Rührmair <markus.ruehrmair@gmail.com>
2023-09-18 22:06:42 +0200

    added vs64 config

---
+++ LatencyTester/src/latencytester.asm
@@ -0,0 +1,26 @@
+﻿  !cpu 6502
+  
+  ;joystick addresses 
+  CIAPRA = $DC00     ;joystick port 2
+  
+  ;startup address SYS 49152
+  * = $C000
+ 
+  LDA #$93  
+  JSR $FFD2              ; clear screen
+  
+  LDA #$01                ; White
+  STA $d021               ; Border
+  STA $d020               ; Background
+  
+readjoystick 
+  LDA CIAPRA              ;** Read port 2
+  AND #$1
+  BEQ joystickmoveup        
+  JMP readjoystick 
+  
+joystickmoveup
+  LDA #$00                ; Black
+  STA $d021               ; Border
+  STA $d020               ; Background
+  RTS
